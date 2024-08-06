7316b4d7a505dda3b815e8921e1a1db3ee266cbd
a.konshin <a.konshin@tinkoff.ru>
2024-08-06 12:51:45 +0300

    Added TCA

---
+++ MergeMaster/Screens/Projects/ProjectsListView.swift
@@ -0,0 +1,20 @@
+//
+//  ProjectsListView.swift
+//  MergeMaster
+//
+//  Created by Aleksey Konshin on 06.08.2024.
+//  Copyright © 2024 Konshin. All rights reserved.
+//
+
+import SwiftUI
+import ComposableArchitecture
+
+struct ProjectsListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProjectsListView()
+}
