d13a6fe84e08ca226402c48f8f2fe6bb064aba24
a.konshin <a.konshin@tinkoff.ru>
2024-12-08 02:31:09 +0300

    Set status bar icon

--- MergeMaster/MenuWizard/MenuWizard.swift
+++ MergeMaster/MenuWizard/MenuWizard.swift
@@ -20,6 +20,7 @@ final class MenuWizard: NSObject {
   private let statusBar: NSStatusBar
   /// Инстанс итема приложения в баре osx
   let statusItem: NSStatusItem
+  private var imageCache: [NSColor: NSImage] = [:]
 
   init(statusBar: NSStatusBar, router: Router<Route>, appState: AppState) {
     self.statusBar = statusBar
@@ -40,14 +41,6 @@ final class MenuWizard: NSObject {
       button.action = #selector(tapToItem(button:))
     }
 
-//    setNumberOfRequests(appState.numberOfRequests.value)
-//
-//    appState.numberOfRequests
-//      .asDriver()
-//      .drive(onNext: { [unowned self] num in
-//        self.setNumberOfRequests(num)
-//      })
-//      .disposed(by: disposeBag)
     setNumberOfRequests(0)
   }
 
@@ -63,13 +56,38 @@ final class MenuWizard: NSObject {
 extension MenuWizard: IMenuWizard {
   /// Отображает количество реквестов на статус баре
   func setNumberOfRequests(_ num: Int) {
+    let color = num > 0 ? NSColor.controlAccentColor : NSColor.labelColor
     let title = NSAttributedString(
-      string: "mr: \(num)",
+      string: "\(num)",
       attributes: [
-        NSAttributedString.Key.foregroundColor: num == 0 ? NSColor.labelColor : NSColor.controlAccentColor
+        NSAttributedString.Key.foregroundColor: color,
+        .font: NSFont.systemFont(ofSize: 13, weight: .medium)
       ]
     )
-    statusItem.attributedTitle = title
+
+    let button = statusItem.button
+    button?.attributedTitle = title
+    button?.image = self.image(color: color)
+  }
+
+  private func image(color: NSColor) -> NSImage? {
+    if let cached = imageCache[color] {
+      return cached
+    } else {
+      guard let image = NSImage(named: "mr_status_icon_16") else { return nil }
+      image.isTemplate = false
+      image.lockFocus()
+
+      color.set()
+
+      let imageRect = NSRect(origin: NSZeroPoint, size: image.size)
+      imageRect.fill(using: .sourceAtop)
+
+      image.unlockFocus()
+      imageCache[color] = image
+
+      return image
+    }
   }
 }
 
